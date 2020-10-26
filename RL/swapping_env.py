7bfc75ecc52df6b127f723c9082df3fcb4c37608
Garyandtang <a750695325@gmail.com>
2020-10-26 22:41:35 +0800

    barrier-unfinished

--- RL/swapping_env.py
+++ RL/swapping_env.py
@@ -17,6 +17,13 @@ from geometry_msgs.msg import PoseStamped, Quaternion, TransformStamped, Twist
 import numpy as np
 import time
 
+from cvxopt import matrix
+from cvxopt.blas import dot
+from cvxopt.solvers import qp, options
+from cvxopt import matrix, sparse
+
+import itertools
+from scipy.special import comb
 class GazeboEnv():
     def __init__(self, robot_name = "robot1/mybot", robot_no = 2):
         self.robot_name = robot_name
@@ -69,21 +76,22 @@ class GazeboEnv():
 
         
         # print("v type: {}".format(type(vel_cmd.linear.x)))
-        time.sleep(self.time_step)
+        # time.sleep(self.time_step)
         rospy.wait_for_service('/gazebo/get_model_state')
 
         for i in range(self.robot_no):
             robot_name = "robot{}/mybot".format(i+1)
+            # print(robot_name)
             if robot_name == self.robot_name:
                 try:
-                    robot_state = self.get_state(self.robot_name, "world")
+                    robot_state = self.get_state(robot_name, "world")
                     self.positions_list[i] = [robot_state.pose.position.x, robot_state.pose.position.y]
                     self.robot_state = robot_state
                 except rospy.ROSInterruptException as e:
                     print("get robot pose fail!")
             else:
                 try:
-                    robot_state = self.get_state(self.robot_name, "world")
+                    robot_state = self.get_state(robot_name, "world")
                     self.positions_list[i] = [robot_state.pose.position.x, robot_state.pose.position.y]
                     # print(self.robot_state.pose.position)
                 except rospy.ROSInterruptException as e:
@@ -117,19 +125,21 @@ class GazeboEnv():
         if d > 2.75:
             done = True
             reward = -50
+        # # deminish w_max and v_max, to find best w_max and v_max
         # if abs(self.pitch) > 0.1:
         #     done = True
         #     reward = -50
-            # self.v_car_max *= 0.99
-            # self.w_max *= 0.99
-            # if self.v_car_max < 1:
-            #     self.v_car_max = 1
-            # if self.w_max < 2.8:
-            #     self.w_max = 2.8
+        #     self.v_car_max *= 0.99
+        #     self.w_max *= 0.99
+        #     if self.v_car_max < 1:
+        #         self.v_car_max = 1
+        #     if self.w_max < 2.8:
+        #         self.w_max = 2.8
         self.state = np.array([d, alpha]+list(self.vel_cmd))
-        # print(self.vel_cmd)
-        # print(self.state)
-        return self.state, reward, done, self.positions_list
+        orientation = self.robot_state.pose.orientation
+        _, _, theta = tf.transformations.euler_from_quaternion(
+            [orientation.x, orientation.y, orientation.z, orientation.w])
+        return self.state, reward, done, self.positions_list, theta
 
 
     
@@ -163,9 +173,10 @@ class GazeboEnv():
         # reset robot pose
         start_position = np.random.uniform(0,2,2)
         start_angle = np.random.uniform(-np.pi, np.pi, 1)
-        self.set_start(start_position[0], start_position[1], start_angle[0])
+        # self.set_start(start_position[0], start_position[1], start_angle[0])
+        self.set_start(1, 0, 0)
         # reset robot goal
-        self.goal = [1,1, np.pi]
+        self.goal = [3,1, np.pi]
         d, alpha = self.cal_relative_pose()
         self.state = np.array([d, alpha]+self.vel_cmd)
         self.d_previous = d
@@ -188,10 +199,12 @@ class GazeboEnv():
         except rospy.ROSInterruptException as e:
             print("set robot start position fail!")
 
+    # predefined trajectory a 2x2 square
     def reset_test_goal(self):
         goal_list = [[0,0], [0,2], [2,2], [2,0]]
         self.goal = goal_list[self.count%4]+[np.pi]
-        
+
+    # randomly reset goal   
     def set_goal(self):
         target_position = np.random.uniform(0,self.length,2)
         return [target_position[0],target_position[1], np.pi] 
@@ -199,25 +212,106 @@ class GazeboEnv():
     def kinetic_constraint():
         return
 
+def si_to_uni_dyi(dx, theta):
+    linear_velocity_gain = 1
+    angular_velocity_limit=np.pi
+    a = np.cos(theta)
+    b = np.sin(theta)
+    dxu = np.zeros((2,1))
+    dxu[0] = linear_velocity_gain*(a*dx[0]+b*dx[1])
+    dxu[1] = angular_velocity_limit*np.arctan2(-b*dx[0] + a*dx[1], dxu[0])/(np.pi/2)
+    return dxu
+
+def si_to_uni_dyi_2(dx, theta):
+    linear_velocity_gain = 1
+    angular_velocity_limit=np.pi
+    a = np.cos(theta)
+    b = np.sin(theta)
+    l = 0.01
+    dxu = np.zeros((2,1))
+    dxu[0] = a*dxu[0] + b*dxu[1] 
+    dxu[1] = -1/l*b*dxu[0] + 1/l*a*dx[1]
+    return dxu
+
+def uni_to_si_dyi(dxu, theta):
+    a = np.cos(theta)
+    b = np.sin(theta)
+    l = 0.01
+    dx = np.zeros((2,1))
+    dx[0] = a*dxu[0] - l*b*dxu[1] 
+    dx[1] = b*dxu[0] + l*a*dx[1]
+    return dx
+
 if __name__ == "__main__":
     env = GazeboEnv()
     xx = env.reset()
     print(xx)
     action = dict(linear_vel=0, angular_vel=0)
-    # state,reward,done ,_ = env.excute(action)
-    # print(env.state)
+
+    r = 0.059
+    l = 0.12
+    J = np.array([[r/2, r/2],[r/l, -r/l]])
+    J_inv = np.linalg.inv(J)
+    v_max = 0
+    w_max = 0
     '''
     Control with PID
     '''
     while 1:
-        state,reward,done ,positions_list = env.excute(action)
-        print(positions_list)
-        # d = state[0]
-        # alpha = state[1]
-        # action["linear_vel"] = 1*d
-        # action["angular_vel"] = 2*alpha
-        # if done:
-        #     action["linear_vel"] = 0
-        #     action["angular_vel"] = 0
-        #     env.reset()
+        state,reward,done ,positions_list, theta = env.excute(action)
+        # print(positions_list)
+        d = state[0]
+        alpha = state[1]
+        v = np.array([0.1*d, 0.16*alpha])
+      
+        # dx = np.zeros((2,1))
+        # dx[0] = v[0]*np.cos(theta)
+        # dx[1] = v[0]*np.sin(theta)
+        dx = uni_to_si_dyi(v, theta)
+        print(v)
+        print(dx)
+        '''
+        barrier function implementation
+        here only consider one control input
+        how to map unicycle control input to si control input is a problem!!
+        '''
+        N = positions_list.shape[0]
+        num_constraints = int(comb(N,2))
+        A = np.zeros((num_constraints, N))
+        b = np.zeros(num_constraints)
+        H = sparse(matrix(2*np.identity(N)))
+        count = 0
+        safety_radius=0.1
+        barrier_gain=100
+        for i in range(N-1):
+            for j in range(i+1, N):
+                error = positions_list[i,:] - positions_list[j,:]
+                h = (error[0]*error[0] + error[1]*error[1]) - np.power(safety_radius, 2)
+
+                A[count, (2*i, (2*i+1))] = -2*error
+                # A[count, (2*j, (2*j+1))] = 2*error
+                b[count] = barrier_gain*np.power(h, 3)
+
+                count += 1
+                
+        f = -2*np.reshape(dx, N, order='F')
+        qp_result = qp(H, matrix(f), matrix(A), matrix(b))['x']
+
+        qp_result = np.reshape(qp_result, (2, -1), order='F')
+        qp_uni_v = si_to_uni_dyi(dx, theta)
+        print(qp_uni_v)
+
+        v_wheel = np.dot(J_inv, qp_uni_v)
+        action["linear_vel"] = v_wheel[0]
+        action["angular_vel"] = v_wheel[1]
+        print(action)
+        if done:
+            action["linear_vel"] = 0
+            action["angular_vel"] = 0
+            env.reset()
         # print(reward)
+        # if v[0] > v_max:
+        #     v_max = v[0]
+        # if v[1] > w_max:
+        #     w_max = v[1]
+        # print([v_max, w_max])
